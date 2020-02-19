021f94dcc913c9666a0e45cbad3b12f8139cd37f
Emanuel Ferreira Prado <emanuelprado@Emanuels-MacBook-Air.local>
2020-02-19 18:13:53 -0300

    refactor(src/styles/themes/default.js): modify default theme colors

--- src/styles/Themes/default.js
+++ src/styles/Themes/default.js
@@ -2,5 +2,7 @@ import chroma from 'chroma-js';
 
 export default {
   primary: chroma('#ffc813'),
-  secondary: chroma('#000'),
+  secondary: chroma('#ffffff'),
+  highlight: chroma('#10CA00'),
+  dark: chroma('#131313'),
 };
