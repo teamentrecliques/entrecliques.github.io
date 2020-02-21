087ccee8eec213311a1837b3226670d890c52e86
Emanuel Prado <emanuel.prado.dev@gmail.com>
2020-02-21 12:46:19 -0300

    feat(mainsectiontitle): add MainSectionTitle component

---
+++ src/components/MainSectionTitle/mainsectiontitle.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Layout from '../Layout';
+import * as S from './styled';
+
+const MainSectionTitle = ({ children }) => (
+  <S.Container>
+    <S.TopText>está buscando os</S.TopText>
+    <S.StrongText className="secondary">Melhores</S.StrongText>
+    <S.StrongText>Resultados</S.StrongText>
+    <S.RegularText>Online?</S.RegularText>
+    <S.StrongText>{children}</S.StrongText>
+  </S.Container>
+);
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default MainSectionTitle;
---
+++ src/components/MainSectionTitle/styled.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+  text-transform: uppercase;
+
+  > p {
+    margin-bottom: 0.2em;
+  }
+`;
+
+export const TopText = styled.p`
+  color: ${props => props.theme.secondary};
+  font-size: 1em;
+  font-weight: 300;
+`;
+
+export const StrongText = styled.p`
+  color: ${props => props.theme.primary};
+  font-size: 4em;
+  font-weight: 700;
+
+  &.secondary {
+    color: ${props => props.theme.secondary};
+  }
+`;
+
+export const RegularText = styled.p`
+  color: ${props => props.theme.secondary};
+  font-size: 4em;
+  font-weight: 400;
+`;
