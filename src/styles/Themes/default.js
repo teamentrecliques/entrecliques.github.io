1ffedd3200fce6ee15485d2854b6256a5813e91e
Emanuel Prado <emanuel.prado.dev@gmail.com>
2020-03-31 17:03:25 -0300

    fix(theme): update primary color

--- src/styles/Themes/default.js
+++ src/styles/Themes/default.js
@@ -1,8 +1,9 @@
 import chroma from 'chroma-js';
 
 export default {
-  primary: chroma('#ffc813'),
+  primary: chroma('#FFB320'),
   secondary: chroma('#ffffff'),
   highlight: chroma('#10CA00'),
   dark: chroma('#131313'),
+  extra: chroma('#FFA50F'),
 };
