8d89406c26870ae9dc8b22db67d8873ab2583a95
Emanuel Prado <emanuel.prado.dev@gmail.com>
2020-03-31 17:02:51 -0300

    feat: add sections to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,22 +6,37 @@ import Button from '../components/Button/button';
 import Menu from '../components/Menu/menu';
 import MainSectionTitle from '../components/MainSection/MainSectionTitle/mainsectiontitle';
 import MainSectionText from '../components/MainSection/MainSectionText/mainsectiontext';
-import Section from '../components/MainSection/mainsection';
+import MainSection from '../components/MainSection/mainsection';
 import BgCurve from '../components/MainSection/BgCurve/bgcurve';
+import Section from '../components/Section/section';
 
 const IndexPage = () => {
   return (
     <Index>
       <SEO title="Home" />
       <Menu />
-      <Section type="main">
+
+      <MainSection type="main">
         <div>
           <MainSectionTitle />
           <MainSectionText />
           <Button>Call to action</Button>
         </div>
-        <BgCurve/>
+        <BgCurve />
+      </MainSection>
+
+      <Section title="o que fazemos">
+
       </Section>
+
+      <Section title="produtos">
+
+      </Section>
+
+      <Section title="clientes">
+
+      </Section>
+
     </Index>
   );
 };
