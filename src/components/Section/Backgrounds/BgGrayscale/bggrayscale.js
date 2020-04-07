530f1a5dedce02559c91593772a079958aa42d85
Emanuel Prado <emanuel.prado.dev@gmail.com>
2020-04-07 16:09:51 -0300

    feat: add arrows background

--- src/components/Section/Backgrounds/BgGrayscale/bggrayscale.js
+++ src/components/Section/Backgrounds/BgGrayscale/bggrayscale.js
@@ -1,6 +1,31 @@
 import React from 'react';
+import BackgroundImage from 'gatsby-background-image';
+import { graphql, useStaticQuery } from 'gatsby';
 import * as S from './bggrayscale.styled';
 
-const BgGrayscale = () => <S.BgGrayscale />;
+const BgGrayscale = () => {
+  const data = useStaticQuery(graphql`
+    query ArrowBgImg {
+      desktop: file(relativePath: { eq: "arrows-bg.png" }) {
+        childImageSharp {
+          fluid(quality: 90) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <S.BgGrayscale>
+      <BackgroundImage
+        Tag="div"
+        className="arrows"
+        fluid={data.desktop.childImageSharp.fluid}
+        backgroundColor="transparent"
+      />
+    </S.BgGrayscale>
+  );
+};
 
 export default BgGrayscale;
--- src/components/Section/Backgrounds/BgGrayscale/bggrayscale.styled.js
+++ src/components/Section/Backgrounds/BgGrayscale/bggrayscale.styled.js
@@ -5,7 +5,29 @@ export const BgGrayscale = styled.div`
   top: 0;
   height: 100vh;
   min-height: 600px;
+  overflow: hidden;
   position: absolute;
   width: 100%;
   z-index: 0;
+
+  .arrows {
+    animation: 3s infinite alternate fade;
+    height: 100%;
+    margin-left: 50vw;
+    width: 50vw;
+  }
+
+  @keyframes fade {
+    from {
+      opacity: 0.2;
+    }
+
+    60% {
+      opacity: 1;
+    }
+
+    to {
+      opacity: 0.2;
+    }
+  }
 `;
