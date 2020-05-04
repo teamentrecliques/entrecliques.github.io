2c94fa1e02182482f1e29b6a0a142866339cff6b
Emanuel Prado <emanuel.prado.dev@gmail.com>
2020-05-04 17:42:06 -0300

    feat(site metatada): add keywords

--- src/config/sitemetadata.config.js
+++ src/config/sitemetadata.config.js
@@ -3,4 +3,5 @@ module.exports = {
   shortName: `Entrecliques`,
   description: `Estratégias digitais com foco em performance. A presença online do seu Negócio com o melhor do Marketing Digital.`,
   author: `@entrecliques`,
+  keywords: `entrecliques,consultoria de marketing,mídias digitais,mídias sociais,adwords,google ads,inteligencia de marketing,google meu negocio,conteudo,marketing pequena empresa`
 }
